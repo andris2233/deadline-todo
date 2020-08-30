6153a7b084d00e6fad50b2c1fff63b29cd755b86
Andris Tsildermanis <yanich.2213@gmail.com>
2020-08-30 13:15:09 +0400

    Initial commit

---
+++ src/views/Create.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="test">
+        <h1>Create</h1>
+    </div>
+</template>
---
+++ src/views/Task.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="test">
+        <h1>Task</h1>
+    </div>
+</template>
